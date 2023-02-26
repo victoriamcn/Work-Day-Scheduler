c12b7822b30881371dfc4b8c1e81b276c05aa1e9
Victoria <vmcnorrill@gmail.com>
2023-02-25 22:26:30 -0500

    JS: codeddisplay() functino to get local storage data and add text content should the page refresh on the user. BUG: now the save function now setting localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,13 @@ function workSchedule() {
   $('.saveBtn').on("click", function (e) {
     //console.log("the click listener works")
     preventDefault(e);
+    // set local storage key, value
     let hourId = $(this).parent().attr("id") //time block ID as each key
     let userTask = $(this).siblings("textarea").val();
-    localStorage.setItem(hourId, userTask)
+    localStorage.setItem(hourId, userTask)   
   })
 
-  //Part 2:  TODO: Add code to apply the past, present, or future class to each time  block by comparing the id to the current hour. 
-  //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes?
-  //How can Day.js be used to get the current hour in 24-hour time?
+  //Function to apply the past, present, or future class by comparing the id to the current hour. 
 
   //loop through each time block// for (let i = 0; i < $('textarea').length; i++) {}
   // //Grab Element by Id
@@ -24,10 +23,10 @@ function workSchedule() {
   //Remove Other Time Classes
   // $('textarea').attr('id').removeClass('.present .past .future');
 
-  //.forEach through each id
+  //Loop through each id
   $('.time-block').each(function () {
     //Grab the time-block div by the id
-    debugger
+    // debugger
     let theHour = $(this).attr('id');
 
     //Conditional to assign past/present/future
@@ -42,7 +41,7 @@ function workSchedule() {
       $(this).addClass('future')
       //REMOVE PAST and PRESENT
       $(this).removeClass('present')
-      $(this).removeClass( 'past')
+      $(this).removeClass('past')
     } else {
       //Set PRESENT
       $(this).addClass('present')
@@ -53,41 +52,28 @@ function workSchedule() {
     //Check the Time every Minute
     setInterval(1000 * 60);
   })
-  // let now =dayjs().hour()
-  // let past = dayjs().isBefore('now', 'hour');
-  // let future = dayjs().isAfter('now', 'hour');
 
-  // if (now) {
-  //   $(this).addClass('.present')
-  // } else if (past) {
-  //   $(this).removeClass('.present').addClass('.past')
-  // } else {
-  //   $(this).removeClass('.present').addClass('.future')
-  // }
-
-
-  // Part 3:  TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
+  //check localStorage in each time block to display
   //HINT: How can the id attribute of each time-block be used to do this?
+  //  should the page reload
+  function display() {
+    //Get key from localStorage
+    let savedUserInput = localStorage.getItem(hourId);
+    //Set the value 
+    $('.description').parent().attr("id").val() = savedUserInput;
+    $('.description').textContent = savedUserInput;
+  }
 
-  // var savedInput = $("timeblock").child("textarea").attr("id");
-  // savedInput.value = localStorage.getItem(hourId)
+display();
 
-  // let cancel;
-  // savedInput.addEventListener("keyup", event => {
-  //   if (cancel) clearTimeout(cancel)
-  //   cancel = setTimeout(() => {
-  //     localStorage.setItem("notes", event.target.value)
-  //   }, 1000)
-  // })
-  //check localStorage in each time block  to display should the page reload
-  // $('schedulecontatiner').on( "load", function() {
-  // // 8am
-  // var hourEight = localStorage.getItem('hour-8') /  console.log(hourEight)
-  // $('hour-8').child("textarea").innerHTML(hourEight) //search id of div that we want. parent traverse(). text add a value
+  // $('schedulecontatiner').on("load", function () {
+  //   // 8am
+  //   var hourEight = localStorage.getItem('hour-8') / console.log(hourEight)
+  //   $('hour-8').child("textarea").innerHTML(hourEight) //search id of div that we want. parent traverse(). text add a value
 
 
 
   //Current Day displayed in the Header
   var today = dayjs().format('[Today is] dddd, MMM D, YYYY');
   $('#currentDay').text(today)
-};
+}
