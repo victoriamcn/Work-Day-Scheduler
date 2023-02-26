275952d548786fcb01207da1ead89d78a67e4966
Victoria <vmcnorrill@gmail.com>
2023-02-26 14:01:49 -0500

    JS: get local storage worked

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,33 +17,6 @@ function workSchedule() {
     localStorage.setItem(hourId, userTask)
   })
 
-
-  //check localStorage to display data should the page reload
-  function inCaseReload() {
-    // loop
-    $('.hour').each(function (){
-      //define
-      let currentHour = (this).text();
-      let currentInput = localStorage.getItem(currentHour)
-      //conditional
-      if (currentInput !== null) {
-        $(this).siblings('.description').val(currentInput)
-      }
-    })
-  }
-
-  // $(#hour-08 .description).val(localStorage.getItem('hour-08'));
-  // $(#hour-09 .description).val(localStorage.getItem('hour-09'));
-  // $(#hour-10 .description).val(localStorage.getItem('hour-10'));
-  // $(#hour-11 .description).val(localStorage.getItem('hour-11'));
-  // $(#hour-12 .description).val(localStorage.getItem('hour-12'));
-  // $(#hour-13 .description).val(localStorage.getItem('hour-13'));
-  // $(#hour-14 .description).val(localStorage.getItem('hour-14'));
-  // $(#hour-15 .description).val(localStorage.getItem('hour-15'));
-  // $(#hour-16 .description).val(localStorage.getItem('hour-16'));
-  // $(#hour-17 .description).val(localStorage.getItem('hour-17'));
-  // $(#hour-18 .description).val(localStorage.getItem('hour-18'));
-
   function timeColor() {
     //Gets Time Accepts numbers from 0 to 23
     let currentTime = dayjs().hour()
@@ -76,6 +49,32 @@ function workSchedule() {
       }
     })
   }
-  function timeColor() //calling the timeColor function
-  function inCaseReload() //calling the inCaseReload function
+
+  //check localStorage to display data should the page reload
+  function inCaseReload() {
+    // loop
+    $('.time-block').each(function () {
+      // $(#hour-08 .description).val(localStorage.getItem('hour-08'));
+      // $(#hour-09 .description).val(localStorage.getItem('hour-09'));
+      // $(#hour-10 .description).val(localStorage.getItem('hour-10'));
+      // $(#hour-11 .description).val(localStorage.getItem('hour-11'));
+      // $(#hour-12 .description).val(localStorage.getItem('hour-12'));
+      // $(#hour-13 .description).val(localStorage.getItem('hour-13'));
+      // $(#hour-14 .description).val(localStorage.getItem('hour-14'));
+      // $(#hour-15 .description).val(localStorage.getItem('hour-15'));
+      // $(#hour-16 .description).val(localStorage.getItem('hour-16'));
+      // $(#hour-17 .description).val(localStorage.getItem('hour-17'));
+      // $(#hour-18 .description).val(localStorage.getItem('hour-18'));
+      //define current hour id by each ID
+      let currentHourId = $(this).attr('id')
+      let currentInput = localStorage.getItem(currentHourId)
+      //conditional
+      if (currentInput !== null) {
+        $(this).children('.description').val(currentInput)
+      }
+    })
+  }
+
+  timeColor() //calling the timeColor function
+  inCaseReload() //calling the inCaseReload function
 }
